1c311ec310ac0712cb7da20ae426146b5d0f3b01
fcoury <felipe.coury@gmail.com>
2010-02-02 01:19:03 +0000

    Allows change_dns and DNS_ALIAS settings

--- scripts/rails.sh
+++ scripts/rails.sh
@@ -1,13 +1,14 @@
 if [[ "$WEB_SERVER" == "apache" ]]; then
   PHD_VIRTUALHOST_TEXT='<VirtualHost *:80>
     ServerName $host
+    ServerAlias $DNS_ALIAS
     DocumentRoot $dir/public
     PassengerAppRoot $dir
   </VirtualHost>'
 else
   PHD_VIRTUALHOST_TEXT='server {
       listen 80;
-      server_name $host;
+      server_name $host $DNS_ALIAS;
       root $dir/public;
       passenger_enabled on;
   }'
