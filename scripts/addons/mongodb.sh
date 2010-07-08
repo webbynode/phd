8f7fbfb11e56dfc99f6cc54e1f40572cd8fc4de2
Felipe Coury <felipe.coury@gmail.com>
2010-07-08 10:42:37 -0300

    Added addon logging infrastructure. Added redis addon.

--- scripts/addons/mongodb.sh
+++ scripts/addons/mongodb.sh
@@ -1,23 +1,22 @@
 check_installed mongodb-stable
 
 if [ $? -eq 1 ]; then
-  echo "  => Installing addon MongoDB"
+  add_on "MongoDB"
 
   # adds 10gen repo to aptitude sources
-  echo "     Adding 10gen repository..."
+  add_on_status "Adding 10gen repository"
   sudo bash -c 'echo "deb http://downloads.mongodb.org/distros/ubuntu 10.4 10gen" >> /etc/apt/sources.list'
 
   # installs 10gen repo GPG key
-  echo "     Acquiring 10gen gpg aptitude key..."
+  add_on_status "Acquiring 10gen gpg aptitude key"
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
   if [ $? != 0 ]; then
     sudo apt-key adv --keyserver pool.sks-keyservers.net --recv 7F0CEB10
   fi
   sudo apt-get update >/var/log/phd/mongodb-install.log 2>&1
 
-  echo "     Installing MongoDB..."
-  echo "     This may take a few minutes, please wait..."
-  install_if_needed mongodb-stable >/var/log/phd/mongodb-install.log 2>&1
+  add_on_wait "Installing MongoDB"
+  install_if_needed mongodb-stable >>/var/log/phd/mongodb-install.log 2>&1
   echo ""
 fi
 
---
+++ scripts/addons/redis.sh
@@ -0,0 +1,29 @@
+if [ -f /etc/init.d/redis ]; then
+  exit 1;
+fi
+
+add_on "Redis"
+
+add_on_status "Installing prerequisite cronolog"
+install_if_needed cronolog >> $LOG_DIR/redis.log 2>&1
+
+cd /usr/src
+add_on_status "Downloading redis"
+sudo wget --quiet http://redis.googlecode.com/files/redis-1.2.6.tar.gz >> $LOG_DIR/redis.log 2>&1
+sudo rm redis-1.2.6.tar.gz
+cd redis-*
+
+add_on_status "Compiling redis"
+sudo make >> $LOG_DIR/redis.log 2>&1
+
+add_on_status "Installing redis"
+sudo cp redis.conf /etc
+
+sudo bash -c 'echo "
+daemonize no
+logfile stdout" >> /etc/redis.conf'
+
+sudo bash -c 'echo "nohup /usr/bin/redis-server /etc/redis.conf | /usr/sbin/cronolog /var/log/redis/redis.%Y-%m-%d.log 2>&1 &" > /etc/init.d/redis'
+
+add_on_status "Starting redis"
+sudo /etc/init.d/redis >> $LOG_DIR/redis.log 2>&1
