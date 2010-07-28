fcbebbbdf1f3e30ac66e9a743948ad0dc6185a30
Felipe Coury <felipe.coury@gmail.com>
2010-07-28 12:07:10 -0300

    Beanstalkd as addon

---
+++ scripts/addons/beanstalkd.sh
@@ -0,0 +1,16 @@
+check_installed beanstalkd
+
+if [ "$installed" == "n" ]; then
+  add_on "Beanstalkd"
+  
+  add_on_wait "Installing Beanstalkd"
+  install_if_needed memcached # >>$LOG_DIR/memcached-install.log 2>&1
+
+  add_on_wait "Configuring Beanstalkd"
+  sudo sed -e 's|#START=yes|START=yes|' -i /etc/default/beanstalkd 
+
+  add_on_wait "Starting Beanstalkd"
+  sudo /etc/init.d/beanstalkd
+  echo ""
+fi
+
