e622d79051836107f6c0938094ff90da93d186c6
Felipe Coury <felipe.coury@gmail.com>
2010-07-28 14:11:58 -0300

    Added better feedback for installation

--- scripts/addons/beanstalkd.sh
+++ scripts/addons/beanstalkd.sh
@@ -3,13 +3,12 @@ check_installed beanstalkd
 if [ "$installed" == "n" ]; then
   add_on "Beanstalkd"
   
-  add_on_wait "Installing Beanstalkd"
-  install_if_needed beanstalkd # >>$LOG_DIR/memcached-install.log 2>&1
+  install_if_needed beanstalkd
 
-  add_on_wait "Configuring Beanstalkd"
+  add_on_status "Configuring Beanstalkd"
   sudo sed -e 's|#START=yes|START=yes|' -i /etc/default/beanstalkd 
 
-  add_on_wait "Starting Beanstalkd"
+  add_on_status "Starting Beanstalkd"
   sudo /etc/init.d/beanstalkd start
   echo ""
 fi
