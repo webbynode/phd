04914985e0b6f8bd9328b8645ffe02e680d46a1e
Felipe Coury <felipe.coury@gmail.com>
2010-07-28 14:03:24 -0300

    Beanstalk install fix

--- scripts/addons/beanstalkd.sh
+++ scripts/addons/beanstalkd.sh
@@ -4,7 +4,7 @@ if [ "$installed" == "n" ]; then
   add_on "Beanstalkd"
   
   add_on_wait "Installing Beanstalkd"
-  install_if_needed memcached # >>$LOG_DIR/memcached-install.log 2>&1
+  install_if_needed beanstalkd # >>$LOG_DIR/memcached-install.log 2>&1
 
   add_on_wait "Configuring Beanstalkd"
   sudo sed -e 's|#START=yes|START=yes|' -i /etc/default/beanstalkd 
