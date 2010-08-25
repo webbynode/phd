21b1ff5764345abd8a0cbfb77990c3b56a9a0503
Felipe Coury <felipe.coury@gmail.com>
2010-08-25 12:51:45 -0300

    PHP with Nginx uses PHP-FPM

--- scripts/php.sh
+++ scripts/php.sh
@@ -36,21 +36,12 @@ else
     echo "Adding PHP support to nginx..."
 
     echo "  => Installing dependencies, this can take a few minutes..."
-    sudo apt-get -y -q install php5-cli php5-cgi php5-mysql php5-pgsql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl lighttpd 2>&1 | sed 's/^/     /'
-
-    sudo /etc/init.d/lighttpd stop > /dev/null 2>&1
-    sudo update-rc.d -f lighttpd remove
-
-    echo "  => Spawning fastcgi..."
-    sudo /usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -u www-data -g www-data -f /usr/bin/php5-cgi -P /var/run/fastcgi-php.pid
-
-    echo "#\!/bin/sh -e
-/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -u www-data -g www-data -f /usr/bin/php5-cgi -P /var/run/fastcgi-php.pid
-" > /tmp/fcgi-php
-
-    sudo mv /tmp/fcgi-php /etc/init.d/fcgi-php
-    sudo chmod +x /etc/init.d/fcgi-php
-    sudo update-rc.d fcgi-php start 51 S .
+    sudo aptitude install python-software-properties 2>&1 | sed 's/^/     /'
+    sudo add-apt-repository ppa:brianmercer/php 2>&1 | sed 's/^/     /'
+    sudo aptitude -y update 2>&1 | sed 's/^/     /'
+    sudo aptitude -y install php5-cli php5-common php5-mysql php5-suhosin php5-gd
+    sudo aptitude -y install php5-fpm php5-cgi php-pear php5-memcache php-apc
+    service php5-fpm start
   
     echo "Done!"
     echo ""
