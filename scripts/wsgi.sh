7c5a14892a7d2f40fb1d0681b12e1aaf58aef08d
Felipe Coury <felipe.coury@gmail.com>
2010-08-25 12:50:00 -0300

    Added WSGI and made Django support use Passenger with Nginx

--- scripts/wsgi.sh
+++ scripts/wsgi.sh
@@ -10,7 +10,6 @@ fi
 
 # django
 install_if_needed python-setuptools
-install_if_needed python-django "sudo easy_install Django"
 
 # for pgsql
 install_if_needed python-psycopg2
@@ -57,51 +56,25 @@ application = django.core.handlers.wsgi.WSGIHandler()
       server_name $host $dns_alias;
       root $dir/public;
       passenger_enabled on;
-
+      
       location /media {
         root /usr/share/pyshared/django/contrib/admin;
       }
   }'
-
+  
   mkdir -p $dir/public
   mkdir -p $dir/tmp
 fi
 
-echo "Configuring Django application..."
+echo "Configuring WSGI application..."
 
 configure_vhost
 already_existed=$?
-
-if [[ -z "$django_username" ]]; then
-  django_username='admin'
-  echo "     WARN: Missing django_username setting, assuming 'admin'"
-fi
-
-if [[ -z "$django_email" ]]; then
-  django_email='admin@example.org'
-  echo "     WARN: Missing django_email setting, assuming 'admin@example.org'"
-fi
 
 echo "  => Configuring database..."
 sudo config_app_db $app_name > $LOG_DIR/config_db.log 2>&1
 
 old_dir=`pwd`
-
-cd $dir
-echo "  => Configuring server side settings.py..."
-/var/webbynode/django/settings.py.sh $app_name 2>&1 | sed 's/^/     /'
-
-echo "  => Sync'ing database..."
-python manage.py syncdb --noinput 2>&1 | sed 's/^/     /'
-
-echo "  => Creating Django superuser..."
-result=`PYTHONPATH=$dir python /var/webbynode/django/create_superuser.py $django_username "$django_email" 2>&1`
-
-if [[ $result =~ 'duplicate key value violates unique constraint "auth_user_username_key"' ]]; then
-  echo "     Already created."
-fi
-
-cd $old_dir
 
 if [[ "$WEB_SERVER" == "apache" ]]; then
   restart_webserver 0
