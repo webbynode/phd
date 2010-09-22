2dfe7ebfc6662ac91097047ebd084405dccecfa7
Felipe Coury <felipe.coury@gmail.com>
2010-09-22 12:24:48 -0300

    Added node.js improvements

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -1,24 +1,33 @@
+echo "------------------------------"
+echo " NodeJS Application Settings:"
+echo "   Use proxy: $nodejs_proxy"
+echo "        Port: $nodejs_port"
+echo "------------------------------"
+echo ""
+
 if [[ "$WEB_SERVER" == "apache" ]]; then
   echo "Node.js Engine is not yet supported in Apache"
 else
+  if [ "$nodejs_proxy" == "Y" ]; then
     PHD_VIRTUALHOST_TEXT='upstream app_cluster_1 {
-      server 127.0.0.1:8000;
-  }
+        server 127.0.0.1:$nodejs_port;
+    }
 
-  server {
-      listen 0.0.0.0:80;
-      server_name $host;
+    server {
+        listen 0.0.0.0:80;
+        server_name $host;
 
-      location / {
-          proxy_set_header X-Real-IP \$remote_addr;
-          proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-          proxy_set_header Host \$http_host;
-          proxy_set_header X-NginX-Proxy true;
+        location / {
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header Host \$http_host;
+            proxy_set_header X-NginX-Proxy true;
 
-          proxy_pass http://app_cluster_1/;
-          proxy_redirect off;
-      }
-  }'
+            proxy_pass http://app_cluster_1/;
+            proxy_redirect off;
+        }
+    }'
+  fi
 
   installing=0
   if [[ ! -x "/usr/local/bin/node" ]]; then
@@ -128,37 +137,53 @@ test -f $dir/$script || {
   exit 1
 }
 
+port_mapping=/var/webbynode/mappings/$nodejs_port.port
+
+if [ -f $port_mapping ]; then
+  port_mapping_app=`cat $port_mapping`
+  if [ ! "$port_mapping_app" == "$app_name" ]; then
+    echo "     ERROR: Port $nodejs_port is already used by application $port_mapping_app"
+    exit 1
+  fi
+
+  rm $port_mapping_app
+  echo $app_name > $port_mapping_app
+
+  # adds a deletion hook
+  mkdir -p /var/webbynode/hooks/delete
+  echo "rm $port_mapping_app" > /var/webbynode/hooks/delete/$app_name
+fi
 
 configure_vhost
 
 echo "  => Configuring database..."
 sudo config_app_db $app_name > $LOG_DIR/config_db.log 2>&1
+check_error 'configuring database' 'config_db'
 
 cd $dir
 
-#script=`ls -1 *.js|tail -1`
-
 echo "  => Configuring monit..."
 sudo rm /etc/monit/services/$app_name > /dev/null 2>&1
+mkdir -p $LOG_DIR/node
 sudo echo "check host $host with address 127.0.0.1
-start program = \"/usr/local/bin/node $dir/$script\"
-stop program  = \"/usr/bin/pkill -f 'node $dir/$script'\"
-if failed port 8000 protocol HTTP
+start program = \"/usr/local/bin/node $dir/$script > $LOG_DIR/node/$app_name.log 2>&1\"
+stop program  = \"/bin/kill \$(ps -eo pid,args | grep $dir/$script | grep -v grep | cut -c1-6)\"
+if failed port $nodejs_port protocol HTTP
     request /
     with timeout 10 seconds
     then restart" > /etc/monit/services/$app_name
     
 echo "  => Starting app..."
 sudo /etc/init.d/monit restart > $LOG_DIR/monit.log 2>&1
+check_error 'configuring database' 'monit'
+
 sleep 3
 sudo monit stop $host
 sleep 5
 sudo monit start $host
 
 sudo chown -R git:www-data * > $LOG_DIR/chown.log 2>&1
-cd -
 
-sudo monit restart $host
 if [ "$installing" == "1" ]; then
   restart_webserver 0
 fi
