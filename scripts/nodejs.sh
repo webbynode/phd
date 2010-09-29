a7d57cb0d3398c4f0f1fdb33db71ffb81730fbfb
Felipe Coury <felipe.coury@gmail.com>
2010-09-29 14:30:39 -0300

    Readded monit integrated with upstart for node.js.

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -165,9 +165,10 @@ sudo config_app_db $app_name > $LOG_DIR/config_db.log 2>&1
 check_error 'configuring database' 'config_db'
 
 cd $dir
+mkdir -p $LOG_DIR/node
 
 echo "  => Configuring upstart..."
-echo "#!upstart
+sudo echo "#!upstart
 description \"$app_name node.js server\"
 author      \"Webbynode Rapp\"
 
@@ -178,30 +179,28 @@ script
     export HOME="$HOME"
 
     exec sudo -u git /usr/local/bin/node $dir/$script 2>&1 >> $LOG_DIR/node/$app_name.log 2>&1
-end script" > /etc/init/$app_name.conf
+end script" > /tmp/$app_name.conf
+sudo mv /tmp/$app_name.conf /etc/init
 sudo chmod +x /etc/init/$app_name.conf
 
-start $app_name
+sudo start $app_name
 
-# echo "  => Configuring monit..."
-# sudo rm /etc/monit/services/$app_name > /dev/null 2>&1
-# mkdir -p $LOG_DIR/node
-# sudo echo "check host $host with address 127.0.0.1
-# start program = \"/usr/local/bin/node $dir/$script >> $LOG_DIR/node/$app_name_monit.log 2>&1\"
-# stop program  = \"/usr/bin/pkill -f 'node $dir/$script'\"
-# if failed port $nodejs_port protocol HTTP
-#     request /
-#     with timeout 10 seconds
-#     then restart" > /etc/monit/services/$app_name
-#     
-# echo "  => Starting app..."
-# sudo /etc/init.d/monit restart > $LOG_DIR/monit.log 2>&1
-# check_error 'configuring database' 'monit'
-# 
-# sleep 3
-# sudo monit stop $host
-# sleep 3
-# sudo monit start $host
+echo "  => Configuring monit..."
+sudo rm /etc/monit/services/$app_name > /dev/null 2>&1
+sudo echo "#!monit
+set logfile $LOG_DIR/node/$app_name_monit.log
+
+check host nodejs with address 127.0.0.1
+    start program = \"/sbin/start $app_name\"
+    stop program  = \"/sbin/stop $app_name\"
+    if failed port $nodejs_port protocol HTTP
+        request /
+        with timeout 10 seconds
+        then restart" > /etc/monit/services/$app_name
+    
+echo "  => Restarting monit..."
+sudo /etc/init.d/monit restart > $LOG_DIR/monit.log 2>&1
+check_error 'restarting monit' 'monit'
 
 sudo chown -R git:www-data * > $LOG_DIR/chown.log 2>&1
 
