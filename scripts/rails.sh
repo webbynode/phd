532662daea65d8e4db419a54b3246bb136ba2654
Felipe Coury <felipe.coury@gmail.com>
2011-02-23 22:32:51 -0300

    Added info message for mysql2 in Rails 2 as well

--- scripts/rails.sh
+++ scripts/rails.sh
@@ -50,6 +50,7 @@ if [ -z "$skipdb" ]; then
     echo "  => Configuring database.yml..."
     sed "s/@app_name@/$name/g" /var/webbynode/templates/rails/database.yml > $dir/config/database.yml
     if [[ ! -z "${rails_adapter}" ]]; then
+      echo "     using adapter: ${rails3_adapter}"
       sed -i "s/adapter: mysql/adapter: ${rails_adapter}/g" $dir/config/database.yml
     fi
   fi
